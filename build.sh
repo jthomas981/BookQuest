94342db260b46541079c9fa13b03cd1adfa1d705
jthomas981 <john.thomas.mouse@gmail.com>
2024-08-27 22:18:05 -0400

    Makemigration happens first ot background_task.

--- build.sh
+++ build.sh
@@ -5,11 +5,12 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Run database migrations
-python manage.py migrate
-
 # Collect static files
 python manage.py collectstatic --no-input
+
+# Run database migrations
+python manage.py makemigrations background_task
+python manage.py migrate
 
 # Start the background task processor
 # Ensure that the background task processor runs in the background
