4b7c20eb3e427950597b8c6521d9cb5925806507
jthomas981 <john.thomas.mouse@gmail.com>
2024-08-27 22:41:07 -0400

    Removed the background refresher.

--- book_quote/views.py
+++ book_quote/views.py
@@ -1,9 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from tasks import reload_website
-
-reload_website()
-
 def homepage(request):
     return render(request, 'homepage.html')
--- build.sh
+++ build.sh
@@ -8,10 +8,5 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --no-input
 
-python manage.py schemamigration background_task --init
 # Run database migrations
 python manage.py migrate 
-
-# Start the background task processor
-# Ensure that the background task processor runs in the background
-python manage.py process_tasks &
