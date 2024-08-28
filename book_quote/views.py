64d782587284abfa7fdc0c999c6d48935a83d316
jthomas981 <john.thomas.mouse@gmail.com>
2024-08-27 21:47:23 -0400

    Added refresh website task.

--- book_quote/views.py
+++ book_quote/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from book_quote.tasks import reload_website
+
+reload_website()
+
 def homepage(request):
     return render(request, 'homepage.html')
