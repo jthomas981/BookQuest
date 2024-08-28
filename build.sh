73c62baf2f2a6bcb52b850aa4d6f47d799ee2075
jthomas981 <john.thomas.mouse@gmail.com>
2024-08-27 22:07:38 -0400

    Changed ordering of the build file.

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
-python manage.py collectstatic --no-input
-
 # Run database migrations
 python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --no-input
 
 # Start the background task processor
 # Ensure that the background task processor runs in the background
